300928b0bfd0a3683ff28e54e7d16bfb01f4d31b
EA1A87 <163140@autistici.org>
2023-04-25 13:59:09 +1000

    Create eac.pl

---
+++ eac.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+#
+# sform - simple script  for music tagging, renaming and converting
+# Copyright (C) 2023  Alexander Makarov
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#===============================================================================
+#        USAGE:  a library
+#  DESCRIPTION:  Exact Audio Copy log parser
+#
+# REQUIREMENTS:  perl 5.36
+#        NOTES:  
+#       AUTHOR:  163140@autistici.org, 
+#      COMPANY:  
+#      CREATED:  25.04.2023 15:15:23
+#     RDEPENDS:  
+#===============================================================================
+
+
+use v5.36
